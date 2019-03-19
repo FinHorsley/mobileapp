65bbd2c570171402c67ca40b9bbcfb2476689026
Desislav Hristov <hristov.desislav@gmail.com>
2019-03-19 04:47:01 +0300

    💄

--- Toggl.Daneel/Presentation/Transition/ModalPresentationController.cs
+++ Toggl.Daneel/Presentation/Transition/ModalPresentationController.cs
@@ -159,12 +159,12 @@ namespace Toggl.Daneel.Presentation.Transition
 
         public override CGSize GetSizeForChildContentContainer(IUIContentContainer contentContainer, CGSize parentContainerSize)
         {
-            var regualrOrCompactHorizontalSizeClass = PresentingViewController.TraitCollection.HorizontalSizeClass == UIUserInterfaceSizeClass.Regular
+            var regularOrCompactHorizontalSizeClass = PresentingViewController.TraitCollection.HorizontalSizeClass == UIUserInterfaceSizeClass.Regular
                                                       || PresentingViewController.TraitCollection.HorizontalSizeClass == UIUserInterfaceSizeClass.Compact;
             var regularVerticalSizeClass = PresentingViewController.TraitCollection.VerticalSizeClass == UIUserInterfaceSizeClass.Regular;
 
             if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad
-                && regualrOrCompactHorizontalSizeClass
+                && regularOrCompactHorizontalSizeClass
                 && regularVerticalSizeClass)
             {
                 var preferredContentHeight = PresentedViewController.PreferredContentSize.Height != 0
@@ -295,13 +295,13 @@ namespace Toggl.Daneel.Presentation.Transition
                 ? UIKeyboard.FrameEndFromNotification(notification).Height
                 : 0.0;
 
-            if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad && finishedPresenting && ContainerView != null && PresentingViewController == null)
+            if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad
+                && finishedPresenting
+                && ContainerView != null
+                && PresentedViewController.PresentedViewController == null)
             {
                 var animationDuration = UIKeyboard.AnimationDurationFromNotification(notification);
-                AnimationExtensions.Animate(animationDuration, Animation.Curves.StandardEase, () =>
-                {
-                    ContainerViewWillLayoutSubviews();
-                });
+                AnimationExtensions.Animate(animationDuration, Animation.Curves.StandardEase, ContainerViewWillLayoutSubviews);
             }
             else if (PresentingViewController == null)
             {
